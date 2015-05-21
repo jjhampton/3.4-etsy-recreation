e568cf990e8526cd485f82c65e65b165f1e3e4d8
John Hampton <john_j_hampton@yahoo.com>
2015-05-21 19:00:29 -0400

    Install Bourbon Bitters for future SCSS styling Add Handlebars.js template to index.html and main.js to dynamically set page title w/ search keyword

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,12 @@
 (function(){
   'use strict';
 
+  //variables assigned to DOM elements
+  var $title = document.querySelector("title");
+
+  //variable representing the URL where the JSONP lives
   var url = "https://api.etsy.com/v2/listings/active.js?api_key=l3usap5o1yhtiz130c55s1fq&keywords=ninjas&includes=Images,Shop&sort_on=score&limit=48";
+
   fetchJSONP(url, app);
 
   //Fetches JSONP data from Etsy API
@@ -22,8 +27,17 @@
 
   //function that will delegate other tasks to helper functions to manipulate data recieved from API
   function app(response) {
+    var keywords = response.params.keywords;
     var results = response.results;
     console.log(results);
+    displayTitle(keywords);
+  }
+
+  function displayTitle(keywords) {
+    var source = document.querySelector("#title-keyword-template").textContent;
+    var template = Handlebars.compile(source);
+    var context = {keyword: keywords};
+    $title.textContent = template(context);
   }
 
 })();
