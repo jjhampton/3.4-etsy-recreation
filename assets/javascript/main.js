b0e3efbeec09ff77dc237b99f912a5f82bc9bdff
John Hampton <john_j_hampton@yahoo.com>
2015-05-26 02:02:08 -0400

    Re-add fetchJSONP function and URL for Etsy API call

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,27 +2,25 @@
 (function(){
   'use strict';
 
-  //variable representing the URL where the JSONP lives
-  // var url = "https://api.etsy.com/v2/listings/active.js?api_key=l3usap5o1yhtiz130c55s1fq&keywords=ninjas&includes=Images,Shop&sort_on=score&limit=24";
-  //
-  // fetchJSONP(url, app);
-  //
-  // //Fetches JSONP data from Etsy API
-  // function fetchJSONP(url, callback) {
-  //   var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-  //   var script = document.createElement('script');
-  //
-  //   window[callbackName] = function(data) {
-  //       delete window[callbackName];
-  //       document.body.removeChild(script);
-  //       callback(data);
-  //   };
-  //
-  //   script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
-  //   document.body.appendChild(script);
-  // }
+//variable representing the URL where the JSONP lives
+  var url = "https://openapi.etsy.com/v2/listings/active.js?api_key=l3usap5o1yhtiz130c55s1fq&keywords=ninjas&includes=Images,Shop&sort_on=score&limit=24";
+  
+  fetchJSONP(url, app);
 
-  app(data);
+  //Fetches JSONP data from Etsy API
+  function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+  }
 
   //function that will delegate other tasks to helper functions to manipulate and display data recieved from API on page
   function app(response) {
